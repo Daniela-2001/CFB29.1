85322fe1e18aaa8bb34db5697445a93997a0091d
Daniela Duarte <danielatavaresd@gmail.com>
2024-02-08 20:23:26 +0000

    CFB29.1

---
+++ aula29.1/Jogador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula29._1
+{
+    internal class Jogador
+    {
+        public int energia = 100;
+
+        public bool vivo = true;
+
+        public string nome;
+
+        public Jogador(string n)
+        {
+            energia = 100;
+            vivo = true;
+            nome = n;
+        }
+    }
+}
---
+++ aula29.1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using aula29._1;
+
+Jogador j1 = new Jogador("Bruno");
+Jogador j2 = new Jogador("Théo");
+
+Console.WriteLine("Nome do jogador 1: {0}", j1.nome);
+Console.WriteLine("Nome do jogador 2: {0}", j2.nome);
